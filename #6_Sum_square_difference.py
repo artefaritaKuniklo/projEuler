346d6f2d0711e5f2b5351b862270783cd48542c2
kuniklo <raven_uw@yahoo.com>
2021-11-05 20:09:55 +0800

    #6

---
+++ #6_Sum_square_difference.py
@@ -0,0 +1,11 @@
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+a = []
+sum = 0
+for i in range(1000):
+    a.append(i+1)
+for ind_a, ax in enumerate(a):
+    for ind_b, bx in enumerate(a):
+        if ind_a != ind_b:
+            sum += (ax*bx)
+print(sum)
